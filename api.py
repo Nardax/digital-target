fe52f39eccffa838686a9ba91fcc682cc087193f
Ryan Pedersen <ryan.pedersen@microsoft.com>
2019-07-09 20:33:55 -0700

    setting env var

--- api.py
+++ api.py
@@ -1,31 +1,12 @@
-import grovepi
-import thread
-import time
-import sys
 from flask import Flask
+import os
 
 app = Flask(__name__)
-
-led = 5
-
-grovepi.pinMode(led, "OUTPUT")
-
-def flash_led(count, duration):
-	for x in range(1, count):
-		grovepi.analogWrite(led, 255)
-		time.sleep(duration)
-		grovepi.analogWrite(led, 0)
-		time.sleep(duration)
-
-def set_led_value(value):
-	grovepi.analogWrite(led, int(value))
 	
 @app.route('/<pinValue>', methods=['POST'])
-def set_led(pinValue):
+def set_pin_value(pinValue):
 	print(type(pinValue))
-	set_led_value(pinValue)
+	os.environ['PIN_VALUE'] = pinValue
 	return pinValue
-
-flash_led(10, .2)
 
 app.run(host='0.0.0.0')
--- detect.py
+++ detect.py
@@ -1,13 +1,37 @@
 import grovepi
+import time
+import os
 
 led = 5 
 sensor = 4
+default_pinValue = 0
 
 grovepi.pinMode(led,"OUTPUT")
-grovepi.analogWrite(led,255)
 
-while True:
-	distance = grovepi.ultrasonicRead(sensor)
-	if distance < 10:
-		grovepi.analogWrite(led,0)
-		print(distance)
+def flash_led(count, duration):
+	for x in range(1, count):
+		set_led_value(255)
+		time.sleep(duration)
+		set_led_value(0)
+		time.sleep(duration)
+
+def set_led_value(value):
+    	grovepi.analogWrite(led, int(value))
+
+def detect_hit():
+	while True:
+		pinValue = os.getenv('PIN_VALUE', default_pinValue)
+		set_led_value(pinValue)
+		if pinValue == 255:
+			distance = grovepi.ultrasonicRead(sensor)
+			if distance < 10:
+				set_led_value(0)
+				os.environ['PIN_VALUE'] = 0
+				report_hit()
+				print(distance)
+
+def report_hit():
+	print("HIT!!!!")
+
+flash_led(10, .2)
+detect_hit()
