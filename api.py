206bc63e7a07a1d3daebcbe6d2593777e36d60ff
Ryan Pedersen <ryan.pedersen@microsoft.com>
2019-07-11 07:12:21 -0700

    clean up errors

--- api.py
+++ api.py
@@ -1,20 +1,20 @@
 from flask import Flask
 import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
-
-print(os.environ['HOME'])
+led = 5 
 
 @app.route('/<pinValue>', methods=['POST'])
 def set_pin_value(pinValue):
-    try:
+	try:
 		conn = sqlite3.connect("data.db")
 		c = conn.cursor()
 		c.execute("UPDATE PinValue SET [Value]=? WHERE Id=?;", (pinValue, led))
 	except Error as e:
 		print(e)
-    finally:
-        conn.close()
+	finally:
+		conn.close()
 	
 	return pinValue
 
